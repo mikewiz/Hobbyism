6880110e14064c2ee1da1b99f6d2b20058fc6a01
mpflint2 <mpflint2@ncsu.edu>
2015-02-23 09:55:37 -0500

    initial Commit

---
+++ Hobbyism/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hobbyism
+//
+//  Created by Michael Flint on 2/23/15.
+//  Copyright (c) 2015 Michael Flint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Hobbyism/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Hobbyism
+//
+//  Created by Michael Flint on 2/23/15.
+//  Copyright (c) 2015 Michael Flint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
